6b88cdfa318e3d4fa6ca9f4099b398092b7b885a
George Spasov <spasov05203@gmail.com>
2020-06-25 23:12:28 +0300

    Added constants for the two messaging systems

--- app/business/handler/types.go
+++ app/business/handler/types.go
@@ -5,11 +5,17 @@ import (
 	"github.com/Limechain/HCS-Integration-Node/app/interfaces/common"
 )
 
-const P2PMessageTypeRFP = "rfp"
-const P2PMessageTypeProposal = "proposal"
-const P2PMessageTypeContractRequest = "contractrequest"
-const P2PMessageTypeContractAccepted = "contractaccepted"
-const P2PMessageTypePO = "po"
+const (
+	P2PMessageTypeRFP              = "rfp"
+	P2PMessageTypeProposal         = "proposal"
+	P2PMessageTypeContractRequest  = "contractrequest"
+	P2PMessageTypeContractAccepted = "contractaccepted"
+	P2PMessageTypePO               = "po"
+)
+const (
+	BlockchainMessageTypeContract = "contract"
+	BlockchainMessageTypePO       = "po"
+)
 
 type BusinessMessageParser interface {
 	Parse(msg *common.Message) (*messages.BusinessMessage, error)
